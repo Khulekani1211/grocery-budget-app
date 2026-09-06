e813a83dc34f0b4d2031129aa6f4efd87c0ef192
Khulekani1211 <khulekanigcaba1211@gmail.com>
2026-09-06 20:18:34 +0200

    New BudgetSection Component

---
+++ src/components/BudgetSection.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import { COLORS } from '../constants/colors.js';
+import Money from './Money.jsx';
+
+export default function BudgetSection({ budget, spent, remaining, onBudgetChange }) {
+    
+}
